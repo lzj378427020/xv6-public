6fe8c15ac59807d32ecfbf428f638bc9922a1c1a
caleb.lv <0577asdfghjkl@163.com>
2019-07-27 00:08:57 +0800

    make a mistake

--- test.c
+++ test.c
@@ -2,10 +2,16 @@
 #include "stat.h"
 #include "user.h"
 
+void test(void)
+{
+    test();
+}
+
 int main(int argc, char* argv[])
 {
     int i = 0;
     printf(1, "pid = %d\n", getpid());
+    test();
     i = 1 / i;
     printf(1, "%d\n", i);
     printf(1, "kill 1 ret = %d\n", kill(1));
