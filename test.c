5ab54c51b29ffad8e6d9b0f8f2909af0f7e81b57
caleb.lv <0577asdfghjkl@163.com>
2019-07-27 00:05:45 +0800

    make a mistake

--- test.c
+++ test.c
@@ -7,6 +7,7 @@ int main(int argc, char* argv[])
     int i = 0;
     printf(1, "pid = %d\n", getpid());
     i = 1 / i;
+    printf(1, "%d\n", i);
     printf(1, "kill 1 ret = %d\n", kill(1));
     exit();
 }
